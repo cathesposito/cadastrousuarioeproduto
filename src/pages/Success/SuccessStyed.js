e7d0f030ec4b270f19c96d42f9b77e51c35777bd
Catarina Mendes <cata_esposito@hotmail.com>
2021-07-24 17:26:25 -0300

    style page success commit

--- src/pages/Success/SuccessStyed.js
+++ src/pages/Success/SuccessStyed.js
@@ -7,18 +7,31 @@ export const Container = styled.main`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+
+  @media (max-width: 900px) {
+    height: 35em;
+  }
 `;
 
 export const ButtonsContainer = styled.section`
   display: flex;
   flex-direction: row;
   margin: 0 auto;
+
+  @media (max-width: 900px) {
+    flex-direction: column;
+    width: auto;
+  }
 `;
 
 export const Label = styled.label`
   text-align: center;
   font-size: 5rem;
   margin-bottom: 20px;
+
+  @media (max-width: 900px) {
+    font-size: 4.5rem;
+  }
 `;
 
 export const Button = styled.button`
@@ -29,4 +42,9 @@ export const Button = styled.button`
   border: none;
   font-size: 4rem;
   margin-left: 10px;
+
+  @media (max-width: 900px) {
+    font-size: 3.5rem;
+    margin-bottom: 10px;
+  }
 `;
